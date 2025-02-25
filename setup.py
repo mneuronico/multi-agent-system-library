8674478b46baf9c75f2054e91aeffd33f0ddf3e2
mneuronico <mneuronico@gmail.com>
2025-02-24 21:28:12 -0300

    version increment

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="mas",
-    version="0.1.11",
+    version="0.1.12",
     license="MIT",
     packages=find_packages(),
     include_package_data=True,
