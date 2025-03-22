14b6132e00afc23fbcd944e4b3f297d3a6d483b8
mneuronico <mneuronico@gmail.com>
2025-03-22 10:53:55 -0300

    Version update

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="mas",
-    version="0.1.19",
+    version="0.1.20",
     license="MIT",
     packages=find_packages(),
     include_package_data=True,
