56d1a7caeee9e72a68eb981eabb9565051112356
mneuronico <mneuronico@gmail.com>
2025-06-10 16:52:24 -0300

    switched from setup.py to pyproject.toml definition

--- setup.py
+++ setup.py
@@ -1,16 +1,4 @@
-from setuptools import setup, find_packages
+# setup.py
+from setuptools import setup
 
-setup(
-    name="mas",
-    version="0.1.20",
-    license="MIT",
-    packages=find_packages(),
-    include_package_data=True,
-    install_requires=[
-        "python-telegram-bot",
-        "requests",
-        "python-dotenv",
-        'backports.zoneinfo; python_version < "3.9"',
-        'tzdata; python_version < "3.9"'
-    ],
-)
+setup()
