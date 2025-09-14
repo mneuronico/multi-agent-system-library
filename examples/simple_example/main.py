64853dcec1faa14bd6be87fc68dc34589962fa18
mneuronico <mneuronico@gmail.com>
2025-09-14 20:05:18 -0300

    Deleted outdated examples

--- examples/simple_example/main.py
+++ examples/simple_example/main.py
@@ -1,7 +1,7 @@
 from mas import AgentSystemManager
 
 # Initialize the manager
-manager = AgentSystemManager(config_json="config.json") 
+manager = AgentSystemManager(config="config.json") 
 output = manager.run(input="Hello world!", verbose=True)
 
 print(output)
